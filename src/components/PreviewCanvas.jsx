96f816a8b68b7faa73da4859191acf0f525bea5a
Monsster <hefestoalan@gmail.com>
2026-03-20 21:25:47 -0300

    add

--- src/components/PreviewCanvas.jsx
+++ src/components/PreviewCanvas.jsx
@@ -4,7 +4,10 @@ const PreviewCanvas = forwardRef(({ data, PreviewComponent }, ref) => {
   return (
     <div ref={ref} className="w-full h-123 shadow-lg overflow-hidden relative flex items-center justify-center bg-black">
       {/* BACKGROUND */}
-      <div className="absolute inset-0 bg-contain bg-center bg-no-repeat" style={{ backgroundImage: "url('/wallpaper.png')" }} />
+     <div
+  className="absolute inset-0 bg-cover bg-center"
+  style={{ backgroundImage: "url('/wallpaper.png')" }}
+/>
 
       {/* OVERLAY */}
       <div className="absolute inset-0 flex items-center justify-center">
